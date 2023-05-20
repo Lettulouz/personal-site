324d5819ef1fb137f25c85f49e92d7e633fb2947
Lettulouz <dominikpiskor1@wp.pl>
2023-05-20 06:43:43 +0200

    Added working demo to shop made in php

--- app/views/home/student/phpShop.php
+++ app/views/home/student/phpShop.php
@@ -21,18 +21,46 @@
     </div>
     <div class="card-body text-black" style="display:none;">
         <h4 class="card-title">Technologie: PHP, JavaScript</h4>
-        <h4 class="card-title">Cel projektu:</h4>
-        <p>Celem projektu było stworzenie systemu CMS. W związku z tym przy wykorzystaniu projektu każdy może stworzyć dowolny sklep definiując samemu zawartość. W projekcie znalazł
-            się panel administratora w bardzo rozbudowanej formie, administrator, oprócz oczywistych czynności, z poziomu przeglądarki jest w stanie edytować ikonę strony, baner 
+        <h4 class="card-title mt-4">Demo: <a href="https://www.mdkcms.dominikpiskor.pl" class="text-muted text-decoration-none"><i class="bi bi-globe"></i> Link</a></h4>
+        <h4 class="card-title mt-4 mb-4">Cel projektu:</h4>
+        <p class="long-text">Celem projektu było stworzenie systemu CMS. W związku z tym przy wykorzystaniu projektu każdy może stworzyć dowolny sklep definiując samemu zawartość. 
+            W projekcie znalazł się panel administratora w bardzo rozbudowanej formie, administrator, oprócz oczywistych czynności, z poziomu przeglądarki jest w stanie edytować ikonę strony, baner 
             znajdujący się na głównej stronie czy chociażby zazwartość stopki. Aspektem brakującym w projekcie do uznania go za produkt gotowy do wydania jest brak podłączenia formy płatności.
         </p>
-        <h4 class="card-title">Skład grupy:</h4>
-        <p>Dominik Piskor</p>
-        <p>Mikołaj Kondziołka</p>
-        <p>Korneliusz Bagiński</p>
-        <p>Klaudia Kołakowska</p>
-        <p>Arkadiusz Gront</p>
-        <p>Wojciech Stachura</p>
+        <h4 class="card-title mt-4 mb-4">Skład grupy:</h4>
+        <p>
+            <i class="bi bi-caret-right-fill"></i> Dominik Piskor
+            <a href="https://github.com/Lettulouz" class="text-muted text-decoration-none"><i class="bi bi-github"></i> @Lettulouz</a>
+        </p>
+        <p>
+            <i class="bi bi-caret-right-fill"></i> Mikołaj Kondziołka
+            <a href="https://github.com/tentegess" class="text-muted text-decoration-none"><i class="bi bi-github"></i> @tentegess</a>
+        </p>
+        <p>
+            <i class="bi bi-caret-right-fill"></i> Korneliusz Bagiński
+            <a href="https://github.com/kBaginskik" class="text-muted text-decoration-none"><i class="bi bi-github"></i> @kBaginskik</a>
+        </p>
+        <p>
+            <i class="bi bi-caret-right-fill"></i> Klaudia Kołakowska
+            <a href="https://github.com/Szpachlarz" class="text-muted text-decoration-none"><i class="bi bi-github"></i> @Szpachlarz</a>
+        </p>
+        <p>
+            <i class="bi bi-caret-right-fill"></i> Arkadiusz Gront
+            <a href="https://github.com/Arek2017" class="text-muted text-decoration-none"><i class="bi bi-github"></i> @Arek2017</a>
+        </p>
+        <p>
+            <i class="bi bi-caret-right-fill"></i> Wojciech Stachura
+            <a href="https://github.com/Kornwaliusz" class="text-muted text-decoration-none"><i class="bi bi-github"></i> @Kornwaliusz</a>
+        </p>
+        <h4 class="card-title mt-4 mb-4">Dokumentacja użytkownika:</h4>
+        <p>
+            <i class="bi bi-caret-right-fill"></i> Link do dokumentacji w języku polskim:
+            <a href="https://github.com/Lettulouz/SI-Bearing-App/blob/master/%5BPL%5DDokumentacja%20u%C5%BCytkownika%20cms.docx" class="text-muted text-decoration-none"><i class="bi bi-github"></i> Dokumentacja</a>
+        </p>
+        <p class="long-text">Dokumentacja zawierająca informacje zarówno dla administratora jak i użytkownika strony o wszystkich najważniejszych aspektach serwisu. 
+            Administrator może w nie odnaleźć treść dotyczącą poprawnego skonfigurowania strony, tak aby poprawnie działała po stronie użytkowników. 
+            Co więcej w treści dokumentacji można znaleźć informacje o zastosowanych bibliotekach zewnętrznych.
+        </p>
     </div>
 </div>
 
