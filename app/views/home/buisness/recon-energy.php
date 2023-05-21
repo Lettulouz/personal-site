1fb88ec105402bf3c806c9148a3a7b8beb494da2
Lettulouz <dominikpiskor1@wp.pl>
2023-05-21 02:34:39 +0200

    Minor change

--- app/views/home/buisness/recon-energy.php
+++ app/views/home/buisness/recon-energy.php
@@ -6,14 +6,8 @@
                     RECON Energy
                 </div>
                 <div class="col-12 col-md-6 d-flex align-items-center justify-content-end">
-                    <div><i class="bi bi-star-fill text-warning ms-1 icons-size"></i></div>
                     <div><i class="bi bi-pencil-fill ms-1 icons-size"></i></div>
-                    <div><i class="bi bi-people-fill ms-1 icons-size"></i></div>
-                    <div class="circle-background1 circle-background-universal ms-1"></div>
-                    <div class="circle-background2 circle-background-universal ms-1"></div>
-                    <div class="circle-background3 circle-background-universal ms-1"></div>
-                    <div class="circle-background0 circle-background-universal ms-1"></div>
-                    <div class="circle-background0 circle-background-universal ms-1"></div>
+                    <div><i class="bi bi-person-fill ms-1 icons-size"></i></div>
                     <div class="arrow ms-3 ml-2"></div>
                 </div>
             </div>
